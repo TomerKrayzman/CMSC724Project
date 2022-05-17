7a95150efdf618715aa9f120216e13b5b6e262a5
nrewkowski <nrewkowski11@gmail.com>
2022-05-17 01:38:56 -0400

    euclidean

--- Assets/XRExploration.cs
+++ Assets/XRExploration.cs
@@ -11,12 +11,25 @@ public class XRExploration : MonoBehaviour
     public LayerMask BVHLayer;
     public List<GameObject> currentPath;
     InfoAnchor[] anchors;
+
+    public float maxDistanceFromAnchors=2.0f;
+
+    public bool sortAnchors=false;
+
+    List<InfoAnchor> results;
+
+    public int numNeighbors=0;
+
+    //mapping of user positions>
+
+
     // Start is called before the first frame update
     void Start()
     {
         //hide all anchors
         anchors=GameObject.FindObjectsOfType<InfoAnchor>();
         print("this many anchors: "+anchors.Length);
+        results=new List<InfoAnchor>();
     }
 
     // Update is called once per frame
@@ -26,10 +39,15 @@ public class XRExploration : MonoBehaviour
         // print(""+BVHLayer);
         currentPath=this.getObjectCurrentBVHPath(Camera.main.gameObject,BVHLayer);
         // print(this.getPathString(currentPath,"CAMERA PATH: "));
-
+        disableAllAnchors();
         //search for the anchors the user should see now by either
-        //1. euclidean distance;no bvh
-        //
+        //1. euclidean distance;no bvh. maybe index?
+        //2. bvh but not tracking where user is; start at the root
+        //3. bvh but starting at user's current node
+        enableAnchorsWithEuclideanDistanceNotIndexed(maxDistanceFromAnchors);
+        print("results: "+percentageCorrect(groundTruthForUserPos(),results)+", "+this.getPathString(currentPath,"CAMERA PATH: ")+", gt="+groundTruthForUserPos().Count);
+
+        //ground truth is how many anchors should actually be visible 
 
         if (Input.GetKeyDown(KeyCode.P)){
             //push current hierarchy to firebase. easier than doing manually
@@ -38,6 +56,50 @@ public class XRExploration : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.L)){
             //temporarily FB grab
         }
+    }
+
+    float percentageCorrect(List<InfoAnchor> groundTruth, List<InfoAnchor> results){
+        int numRight=0;
+        foreach (InfoAnchor i in groundTruth){
+            if (results.Contains(i)){
+                numRight++;
+            } 
+        }
+        return ((float) numRight)/((float)groundTruth.Count);
+    }
+
+    List<InfoAnchor> groundTruthForUserPos(){
+        List<InfoAnchor> groundTruthAnchors=new List<InfoAnchor>();//=//new List<InfoAnchor>(currentPath[0].GetComponentsInChildren<InfoAnchor>());
+
+        for (int j=0; j< currentPath[0].transform.childCount; j++){
+            if (currentPath[0].transform.GetChild(j).gameObject.GetComponent<InfoAnchor>()){
+                groundTruthAnchors.Add(currentPath[0].transform.GetChild(j).gameObject.GetComponent<InfoAnchor>());
+            }
+        }
+        print("Ground truth anchors");
+        if (numNeighbors>0){
+            //get all other children of currentpath[1]
+            //enable all of their nodes
+            //don't worry about contiguous rn
+        }
+        return groundTruthAnchors;
+    }
+
+    void disableAllAnchors(){
+        foreach (InfoAnchor i in anchors){
+            i.gameObject.SetActive(false);
+        }
+    }
+
+    int enableAnchorsWithEuclideanDistanceNotIndexed(float maxDistance){
+        results.Clear();
+        foreach (InfoAnchor i in anchors){
+            if ((i.transform.position-Camera.main.transform.position).magnitude<maxDistance){
+                i.gameObject.SetActive(true);
+                results.Add(i);
+            }
+        }
+        return anchors.Length; //needs to check every anchor
     }
 
 
